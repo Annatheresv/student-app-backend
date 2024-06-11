41ff6bc04bf1b2a5ebaecafc2716f063ca612ac9
Annatheresv <annatheresv@gmail.com>
2024-06-11 10:51:15 +0530

    project created

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-const express = require("express")
+const express  = require("express")
 const mongoose = require("mongoose")
 const cors=require("cors")
-const student=require("./models/student")
+const {studentmodel}=require("./models/student")
 
 const app=express()
 app.use(cors())
 app.use(express.json())
   app.post("/add",(req,res)=>{
     let input = req.body
-    console.log(input)
+    let student=new studentmodel(input)
+    student.save()
+    console.log(student)
     res.send("test")
   })
 app.listen(8080,()=>{
